f044f83a42bb9f214ed6a4f8866ce65c5606dd54
AraceliGS <aragutarras@gmail.com>
2017-11-13 17:03:01 -0500

    añadiendo archivo js corregido según el formato de estilo dado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//var ul = document.getElementsByTagName('ul')[0];
+// var ul = document.getElementsByTagName('ul')[0];
 // var ul = document.querySelectorAll('ul')[0];
 // var ul = document.querySelector('ul');
 // var ul = document.getElementById('ul-id');
